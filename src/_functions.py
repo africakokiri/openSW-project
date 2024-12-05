2d8905f7438437c4d942c954cec532b8a4763768
Choi Chan Hwan <africakokiri@gmail.com>
2024-12-05 09:46:17 +0900

    사이드바를 렌더링하는 write_sidebar 함수 추가
    - _main.py 파일 내의 pages 배열에 정의된 title, file_name을 사이드바에 렌더링하는 wirte_sidebar 함수를 추가했습니다.

--- src/_functions.py
+++ src/_functions.py
@@ -20,3 +20,11 @@ def geocode_address(address):
         return location.latitude, location.longitude
     else:
         return None, None
+
+
+# pages 배열에 추가한 사이드바 객체 렌더링
+def write_sidebar(pages):
+    st.navigation(
+        [st.Page("introduction.py", title="프로젝트 소개")]
+        + [st.Page(page["file_name"], title=page["title"]) for page in pages]
+    ).run()
--- src/_main.py
+++ src/_main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from _functions import write_sidebar
 
 
 # 사이드바에 추가할 페이지
@@ -6,8 +7,4 @@ pages = [
     {"title": "세대별 인기 관광지 분석", "file_name": "popular_place_per_gen.py"},
 ]
 
-# pages 배열에 추가한 사이드바 객체 렌더링
-st.navigation(
-    [st.Page("introduction.py", title="프로젝트 소개")]
-    + [st.Page(page["file_name"], title=page["title"]) for page in pages]
-).run()
+write_sidebar(pages)
