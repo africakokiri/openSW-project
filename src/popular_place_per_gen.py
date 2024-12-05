9f2dd6b9321fae98dba14d311ade039d5f4af709
Choi Chan Hwan <africakokiri@gmail.com>
2024-12-05 09:39:50 +0900

    필요한 코드를 제외한 다른 테스트 코드 삭제
    - 앱 구동에 필요한 코드를 제외한 다른 테스트 코드를 삭제했습니다.

--- src/popular_place_per_gen.py
+++ src/popular_place_per_gen.py
@@ -1,23 +1,10 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
-import streamlit.components.v1 as components
 
 from _functions import write_csv
 
-st.markdown("## 세대별 인기 관광지 분석")  # 페이지 제목
-components.iframe("https://google.com", height=500)
+st.markdown("## 세대별 인기 관광지 분석")
 
-st.markdown(
-    """
-    <style>
-        .stMain{
-            width: 1200px !important;
-        }
-    </style>
-""",
-    unsafe_allow_html=True,
-)
 
 tab_20, tab_30, tab_40, tab_50, tab_60, tab_all = st.tabs(
     ["20대", "30대", "40대", "50대", "60대", "모든 세대"]
