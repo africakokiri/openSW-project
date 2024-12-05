360235bce5b3996304ed7186317485b7943bf7d9
Choi Chan Hwan <africakokiri@gmail.com>
2024-12-05 18:56:53 +0900

    csv파일 교체 및 코드 리팩토링
    - 인코딩 문제가 있는 csv 파일들을 교체했습니다.
    - popular_place_per_gen.py 파일의 코드를 리팩토링했습니다.

--- src/popular_place_per_gen.py
+++ src/popular_place_per_gen.py
@@ -4,6 +4,7 @@ import pandas as pd
 from _functions import write_csv
 
 st.markdown("## 세대별 인기 관광지 분석")
+selected_columns = ["순위", "관심지점명", "주소도로명", "세대검색건수"]
 
 
 tab_20, tab_30, tab_40, tab_50, tab_60, tab_all = st.tabs(
@@ -13,38 +14,35 @@ tab_20, tab_30, tab_40, tab_50, tab_60, tab_all = st.tabs(
 with tab_20:
     write_csv(
         "csv/popular_place_per_gen_2.csv",
-        ["순위", "관심지점명", "주소도로명", "세대검색건수"],
+        selected_columns,
     )
-
-    df = pd.DataFrame([[37.8835, 127.73831]], columns=["lat", "lon"])
-    st.map(df)
 
 with tab_30:
     write_csv(
         "csv/popular_place_per_gen_3.csv",
-        ["순위", "관심지점명", "주소도로명", "세대검색건수"],
+        selected_columns,
     )
 
 with tab_40:
     write_csv(
         "csv/popular_place_per_gen_4.csv",
-        ["순위", "관심지점명", "주소도로명", "세대검색건수"],
+        selected_columns,
     )
 
 with tab_50:
     write_csv(
         "csv/popular_place_per_gen_5.csv",
-        ["순위", "관심지점명", "주소도로명", "세대검색건수"],
+        selected_columns,
     )
 
 with tab_60:
     write_csv(
         "csv/popular_place_per_gen_6.csv",
-        ["순위", "관심지점명", "주소도로명", "세대검색건수"],
+        selected_columns,
     )
 
 with tab_all:
     write_csv(
         "csv/popular_place_per_gen_all.csv",
-        ["순위", "관심지점명", "주소도로명", "세대검색건수"],
+        selected_columns,
     )
